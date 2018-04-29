93709fe6d95752fa7d9fdb8e04033155980946a9
Unknown <verner@fortelius.net>
2018-04-29 16:35:57 +0300

    loadingbutton

--- src/components.js
+++ src/components.js
@@ -1,3 +1,3 @@
-﻿import React, { Component } from 'react';
+﻿import React from 'react';
 
 export const ButtonLoading = ({ loading: Loading, value: Value, ...rest }) => <button {...rest} disabled={Loading}> {Loading && <span><i className="fas fa-circle-notch fa-spin"></i></span>} {Value}</ button>;
