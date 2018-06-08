399a6f7733422138542ada32c43d3edda39739c4
Unknown <verner@fortelius.net>
2018-06-09 00:21:48 +0300

    trigger validation from form

--- src/Shared/TextInputValidated.js
+++ src/Shared/TextInputValidated.js
@@ -3,11 +3,11 @@ import ValidatedInput from './ValidatedInput';
 
 class TextInputValidated extends ValidatedInput {
     render() {
-        const { children, customValidator, onChange, ...rest } = this.props;
+        const { children, customValidator, isFormTouched, onChange, ...rest } = this.props;
         return (
             <div className={this.props.required ? 'form-group required' : 'form-group'}>
                 <label htmlFor={this.props.name}>{this.props.label} {!this.props.required && <small>(Optional)</small>}</label>
-                <input onBlur={this.setValidity} className={this.state.hasError ? 'form-control is-invalid' : 'form-control'} {...rest} onChange={this.handleChange} />
+                <input onBlur={this.setValidity} className={this.state.hasError ? 'form-control is-invalid' : 'form-control'} {...rest} onChange={this.handleChange} ref={this.inputRef} />
                 <div className="invalid-feedback">
                     {this.state.validity.valueMissing && 'This field is required'}
                     {this.state.validity.customError && this.state.errorMessage}
--- src/Shared/ValidatedInput.js
+++ src/Shared/ValidatedInput.js
@@ -1,15 +1,26 @@
-﻿import { Component } from 'react';
+﻿import React, { Component } from 'react';
 
 class ValidatedInput extends Component {
     constructor(props) {
         super(props);
 
+        this.inputRef = React.createRef();
         this.state = {
             touched: false,
             hasError: false,
             validity: {},
+            formTouched: false,
             errorMessage: ''    // todo refactor used for server side error messages... probably needs an array
         };
+    }
+
+
+    componentWillReceiveProps = (props) => {
+        if (!this.state.formTouched && props.isFormTouched) {
+            this.setState({ formTouched: true });
+            console.debug('form touched, do something!');
+            this.checkValidity(this.inputRef.current);
+        }
     }
 
 
@@ -19,11 +30,16 @@ class ValidatedInput extends Component {
     }
 
 
+    // todo fix these names...
     setValidity = async (e) => {
+        this.checkValidity(e.target);
+    }
+
+    // todo fix these names...
+    checkValidity = async (target) => {
         //console.debug(e.target.validity.valueMissing);
         //console.debug(e.target.validity);
         //console.debug(e.target.validity.customError)
-        const target = e.target;
         if (this.props.customValidator) {
             // todo custom validators should return boolean valid and an optional message
             // todo refactor
