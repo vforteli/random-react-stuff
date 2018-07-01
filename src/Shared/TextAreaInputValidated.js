a9ee9ccb3c77f8a8fc3cf187792e9fcc25464fcd
Unknown <verner@fortelius.net>
2018-07-01 23:26:22 +0300

    only pass rest to textarea

--- src/Shared/TextAreaInputValidated.js
+++ src/Shared/TextAreaInputValidated.js
@@ -8,7 +8,7 @@ class TextAreaInputValidated extends ValidatedInput {
         return (
             <div className={this.props.required ? 'form-group required' : 'form-group'}>
                 <label htmlFor={this.props.name}>{this.props.label} {!this.props.required && <small>(Optional)</small>}</label>
-                <textarea onBlur={this.checkValidity} {...rest} className={this.state.hasError ? 'form-control is-invalid' : 'form-control'} {...this.props} onChange={this.handleChange} />
+                <textarea onBlur={this.checkValidity} {...rest} className={this.state.hasError ? 'form-control is-invalid' : 'form-control'} {...rest} onChange={this.handleChange} />
                 <div className="invalid-feedback">
                     This field is required
                 </div>
