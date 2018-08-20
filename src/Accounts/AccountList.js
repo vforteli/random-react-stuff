a7328745b8e19d346a6897f0938a76b3aee18333
Unknown <verner@fortelius.net>
2018-08-20 19:44:19 +0300

    debounced search

--- src/Accounts/AccountList.js
+++ src/Accounts/AccountList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PaginationControl from 'flexinets-reactstrap-pagination';
 import axios from 'axios';
 import qs from 'qs';
+import debounce from 'debounce-promise';
 
 
 
@@ -25,6 +26,7 @@ class AccountList extends React.Component {
     }
 
     async fetchAccounts() {
+        console.debug('fetching accounts');
         const response = await axios.get(`/Api/v2/CrmAccounts/?page=${this.state.currentPage}&search=${this.state.searchString}`);
         this.setState({
             accounts: response.data.Accounts,
@@ -33,11 +35,27 @@ class AccountList extends React.Component {
     }
 
 
+    handleSearch = (e) => {
+        this.setState({ searchString: e.target.value, currentPage: 1 }, () => {
+            this.debouncedSearch(this.state.searchString);
+        });
+    }
+
+
+    debouncedSearch = debounce(async (value) => {
+        console.debug(`Searching for: ${value}`);
+        await this.fetchAccounts();
+    }, 700, { leading: true });
+
+
+
     pageChanged = async (page) => {
         console.debug(`Yay page changed to ${page}`);
-        this.setState({ currentPage: page });
-        await this.fetchAccounts();
-        this.props.history.push(`/accounts?page=${page}`);
+        this.setState({ currentPage: page }, async () => {
+            await this.fetchAccounts();
+            this.props.history.push(`/accounts?page=${page}`);
+        });
+
     }
 
     render() {
@@ -55,6 +73,21 @@ class AccountList extends React.Component {
                 {this.state.accounts !== null &&
                     <div className="card mb-3">
                         <div className="card-body">
+
+                            <div className="row">
+                                <div className="col-sm-9">
+                                    <a ui-sref="accounts.detail.create" className="btn btn-primary"><i className="fas fa-plus"></i> New account</a>
+                                </div>
+                                <div className="col-sm-3">
+                                    <span className="input-group">
+                                        <input name="Search" type="search" value={this.state.searchString} onChange={this.handleSearch} className="form-control" placeholder="Search accounts..." />
+                                        <span className="input-group-btn">
+                                            <button type="submit" ng-click="search(model.searchterm)" className="btn btn-default"><i className="fas fa-search"></i></button>
+                                        </span>
+                                    </span>
+                                </div>
+                            </div>
+
                             <PaginationControl totalCount={this.state.count} pageSize={this.state.pageSize} maxSize={10} currentPage={this.state.currentPage} pageChanged={this.pageChanged} />
                             <table className="table table-hover users-table">
                                 <thead>
@@ -84,7 +117,7 @@ class AccountList extends React.Component {
                     </div>
                 }
             </div>
-        )
+        );
     }
 }
 
